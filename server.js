ca9649182dfa6bf27c340c99b8e257118b57d246
Emanuel Neves <emanuelneves10@gmail.com>
2017-08-16 13:02:08 -0400

    added routing and views folder

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) =>{
-  res.send('hello world!')
-})
+  res.render('home')
+});
 
 app.listen(8080, ()=>{
   console.log('Listening on port 8080...')
-})
+});
