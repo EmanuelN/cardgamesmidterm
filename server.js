fa3dcbd10c87686d20e42d5cf6e55ec10d818b73
Emanuel Neves <emanuelneves10@gmail.com>
2017-08-16 13:59:15 -0400

    fixed server.js and removed ejs

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.set('view engine', 'ejs');
+// const cookieParser = require('cookie-session');
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'public')));
+
 
 app.get('/', (req, res) =>{
-  res.render('home')
+  res.redirect('index.html')
 });
 
 app.listen(8080, ()=>{
