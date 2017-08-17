ebd25558756781cad92553d3ce081102cafc0a0e
EmanuelN <emanuelneves10@gmail.com>
2017-08-17 13:23:13 -0400

    added module for knex and callback for data

--- server/index.js
+++ server/index.js
@@ -17,81 +17,44 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
+const knexhelper = require('../modules/knexhelper.js');
 
-require('dotenv').config();
 
-const knex = require('knex')({
-  client: 'pg',
-  connection: {
-      host     : process.env.DB_HOST,
-      user     : process.env.DB_USER,
-      password : process.env.DB_PASS,
-      database : process.env.DB_NAME,
-      port     : process.env.DB_PORT,
-      // ssl      : process.env.DB_SSL
-  }
-});
-let name = ""
-knex.select('name').from('users').where({id: 1})
-  .asCallback((err, rows) => {
-    if (err){
-    console.error(err)
-  } else{
-    name = rows[0].name;
-  }
-    return knex.destroy();
-});
 
-let rankingBlackjack = {};
-knex.select('name as player').count('winner_id as wins')
-.from('users')
-.innerJoin('blackjacks', 'users.id', 'winner_id')
-.groupBy('name').orderBy('wins', 'desc')
-.asCallback ((err, rows) => {
-  if (err){
-    console.error(err);
-  } else {
-    for (let row in rows){
-      rankingBlackjack[row] = {
-        player: rows[row].player,
-        wins: rows[row].wins
-      };
-    }
-  }
-  return knex.destroy();
-});
 
-let rankingGoofspeil = {};
-knex.select('name as player').count('winner_id as wins')
-.from('users')
-.innerJoin('goofspeils', 'users.id', 'winner_id')
-.groupBy('name').orderBy('wins', 'desc')
-.asCallback ((err, rows) => {
-  if (err){
-    console.error(err);
-  } else {
+//ROUTES
+app.get("/", (req,res) =>{
+  let rankingGoofspeil = {};
+  let myName = ""
+  let rankingBlackjack = {};
+  knexhelper.goofspeil(function(err, rows){
     for (let row in rows){
       rankingGoofspeil[row] = {
         player: rows[row].player,
         wins: rows[row].wins
       };
     }
-  }
-  return knex.destroy();
-});
-
-
-//ROUTES
-app.get("/", (req,res) =>{
-  if (req.session.user_id){
-    res.render('home',
-      {name: name,
-        rankingBlackjack: rankingBlackjack,
-        rankingGoofspeil: rankingGoofspeil
-    });
-  } else{
-    res.render('login');
-  }
+    knexhelper.name(function(err, name){
+      myName = name;
+      knexhelper.blackjack((err, rows)=>{
+        for (let row in rows){
+          rankingBlackjack[row] = {
+            player: rows[row].player,
+            wins: rows[row].wins
+          };
+        }
+        if (req.session.user_id){
+          res.render('home',
+            {name: myName.name,
+              rankingBlackjack: rankingBlackjack,
+              rankingGoofspeil: rankingGoofspeil
+            });
+        } else{
+          res.render('login');
+        }
+      });
+    })
+ });
 });
 
 app.get('/login', (req, res) =>{
