dabbbee884a306e542aa15cccf76dd2e3d80d85e
bhavc <bhavdip@gmail.com>
2017-08-18 13:06:11 -0400

    working on card rendering

--- blackJack.js
+++ blackJack.js
@@ -1,23 +1,28 @@
-house = {
-  score: [],
-  cards: []
-}
+//figure out how to score ace
+//ace = 11, is you're over then subtract 10 from the total 
 
-p1 = {
-  score: [],
-  cards: []
-}
-
-p2 = {
-  score: [],
-  cards: []
+gameState = {
+  house : {
+    score: 0,
+    cards: []
+  },
+  p1 : {
+    score: 0,
+    cards: ['']
+  },
+  p2 : {
+    score: 0,
+    cards: ['']
+  }
 }
 
 suits = ['hearts', 'spades', 'clubs', 'diamonds']
 cards = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']
 
+
+//if the total score is over 21 and you have an ace, subtract 10
 var translateTable = {
-  'A': 1,
+  'A': 11,
   '2': 2,
   '3': 3,
   '4': 4,
@@ -36,24 +41,33 @@ function calcCard(card) {
   card = translateTable[card] || card;
   return card
 }
-//should you push in card or value here?
-function generateNewCardValue(someObject) {
+
+function score(player) {
+  total = 0
+  for (i = 0; i < gameState[player].score.length; i++) {
+    total += gameState[player].score[i]
+  }
+  return total
+}
+
+//everytime user clicks on "hit", generateNewCardValue will be called
+//card needs to be changed into either an array of arrays
+// or object of arrays or object of object
+function generateNewCardValue(someObject, player) {
   value = cards[Math.floor(Math.random() * 13)]
   suit = suits[Math.floor(Math.random() * 4)]
-  someObject.cards.push(suit)
-  someObject.score.push(value)
+  card = `${value} of ${suit}`
+  someObject[player].cards.push(card)
+  someObject[player].score += calcCard(value)
   return someObject
 }
 
+//game will automatcally return the house score
 function houseScore(houseObject) {
-  while (houseObject.score < 17) {
-    generateNewCardValue(houseObject)
+  while (houseObject.house.score < 17) {
+    generateNewCardValue(houseObject, 'house')
   }
   return houseObject
 }
 
-//has to be an event here for "hit"
-function userScores(playerObject) {
-  generateNewCardValue(playerObject)
-  return playerObject
-}
+console.log(houseScore(gameState))
