fd77a6f978e7cf6733095a945741e3547de27255
Avleen <avleen3@gmail.com>
2017-08-20 06:45:14 +0000

    more styling

--- public/scripts/goofspeil.js
+++ public/scripts/goofspeil.js
@@ -1,31 +1,3 @@
-//on click, the card disappears
-$('form[name="register"]').on("submit", function (e) {
-        // Find all <form>s with the name "register", and bind a "submit" event handler
-
-        // Find the <input /> element with the name "username"
-        var username = $(this).find('input[name="username"]');
-        if ($.trim(username.val()) === "") {
-            // If its value is empty
-            e.preventDefault();    // Stop the form from submitting
-            $("#formAlert").slideDown(400);    // Show the Alert
-        } else {
-            e.preventDefault();    // Not needed, just for demonstration
-            $("#formAlert").slideUp(400, function () {    // Hide the Alert (if visible)
-                alert("Would be submitting form");    // Not needed, just for demonstration
-                username.val("");    // Not needed, just for demonstration
-            });
-        }
-    });
-
-    $(".alert").find(".close").on("click", function (e) {
-        // Find all elements with the "alert" class, get all descendant elements with the class "close", and bind a "click" event handler
-        e.stopPropagation();    // Don't allow the click to bubble up the DOM
-        e.preventDefault();    // Don't let any default functionality occur (in case it's a link)
-        $(this).closest(".alert").slideUp(400);    // Hide this specific Alert
-    });
-});
-
-
 $(document).ready(function() {
   var app = {
     cards: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13],
