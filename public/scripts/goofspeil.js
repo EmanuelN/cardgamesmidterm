5f6130ef825677108ee7abbd95277bef75db3870
Avleen <avleen3@gmail.com>
2017-08-20 06:13:15 +0000

    included alert if username empty

--- public/scripts/goofspeil.js
+++ public/scripts/goofspeil.js
@@ -1,15 +1,27 @@
 //on click, the card disappears
-$(document).ready(function(){
-  $('.player-card-deck').on('click', function() {
-    $(this).animate({
-      opacity: 0
-    }).removeClass('.player-card-deck');
-  })
-})
+$('form[name="register"]').on("submit", function (e) {
+        // Find all <form>s with the name "register", and bind a "submit" event handler
 
-$(document).ready(function(){
-    $(".player-card-deck").click(function(){
-        $(this).hide(".player-card-deck");
+        // Find the <input /> element with the name "username"
+        var username = $(this).find('input[name="username"]');
+        if ($.trim(username.val()) === "") {
+            // If its value is empty
+            e.preventDefault();    // Stop the form from submitting
+            $("#formAlert").slideDown(400);    // Show the Alert
+        } else {
+            e.preventDefault();    // Not needed, just for demonstration
+            $("#formAlert").slideUp(400, function () {    // Hide the Alert (if visible)
+                alert("Would be submitting form");    // Not needed, just for demonstration
+                username.val("");    // Not needed, just for demonstration
+            });
+        }
+    });
+
+    $(".alert").find(".close").on("click", function (e) {
+        // Find all elements with the "alert" class, get all descendant elements with the class "close", and bind a "click" event handler
+        e.stopPropagation();    // Don't allow the click to bubble up the DOM
+        e.preventDefault();    // Don't let any default functionality occur (in case it's a link)
+        $(this).closest(".alert").slideUp(400);    // Hide this specific Alert
     });
 });
 
